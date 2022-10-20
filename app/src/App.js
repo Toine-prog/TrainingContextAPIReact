74c7ec5a068b1daa1ac09c062c6787edce8a4023
Antoine Ruchot <a.ruchot@outlook.com>
2022-10-20 19:44:11 -0400

    récupération des infos de l'api ~ok

--- app/src/App.js
+++ app/src/App.js
@@ -8,6 +8,7 @@ import React from 'react';
 import { Home, Page1, Page2 } from './pages';
 import { NavMenu } from './components';
 import { useDarkMode } from './useDarkMode';
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 
 const ThemeContext = React.createContext(null);
@@ -16,19 +17,22 @@ export { ThemeContext};
 function App() {
 
   const {theme, changeMode} = useDarkMode()
+  const client = new QueryClient();
 
   return (
     <ThemeContext.Provider value={{theme, changeMode}}>
-    <div className="App" style={{background: theme.background, color: theme.foreground}}>
-      <Router>
-        <NavMenu />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/page1' element={<Page1 />} />
-          <Route path='/page2' element={<Page2 />} />
-        </Routes>
-      </Router>
-    </div>
+    <QueryClientProvider client={client}>
+      <div className="App" style={{background: theme.background, color: theme.foreground}}>
+        <Router>
+          <NavMenu />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/page1' element={<Page1 />} />
+            <Route path='/page2' element={<Page2 />} />
+          </Routes>
+        </Router>
+      </div>
+    </QueryClientProvider>
     </ThemeContext.Provider>
   );
 }
--- app/src/Pages/Page1.js
+++ app/src/Pages/Page1.js
@@ -1,14 +1,109 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const cities = [
+  {
+    code: "FR",
+    name: "Paris",
+  },
+  {
+    code: "CM",
+    name: "Londres",
+  },
+  {
+    code: "CA",
+    name: "Toronto",
+  },
+  {
+    code: "CN",
+    name: "Pekin",
+  },
+  {
+    code: "ID",
+    name: "Jakarta",
+  },
+  {
+    code: "NO",
+    name: "Oslo",
+  },
+];
+const WEATHER_API_KEY = "eaea3a0c292ca39cb5e5272736989424";
+
+const kelvinToDegre = (kelvin) => {
+  console.log("kelvin", kelvin);
+  return (kelvin - 273).toFixed(1);
+};
+// Afficher la météo d'un pays aléatoirement
 
 const Page1 = () => {
+  const [selectedCity, setSelectedCity] = useState("FR");
+
+  //récupérer la lat et lon de la ville
+  const {
+    data: latLon,
+    isLoading: isLatLonLoading,
+    refetch: refetchLatLon,
+  } = useQuery(["latlng"], async () => {
+    return await Axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${selectedCity}&appid=${WEATHER_API_KEY}`
+    ).then((res) => res.data[0]);
+  });
+
+  let lat = latLon?.lat;
+  let lon = latLon?.lon;
+
+  //Retourner des infos sur la météo
+  const {
+    data: meteoData,
+    isLoading: isMeteoLoading,
+    refetch: refetchMeteo,
+  } = useQuery(["meteo"], async () => {
+    const data = await Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${
+        lat ? lat : 0
+      }&lon=${lon ? lon : 0}&appid=${WEATHER_API_KEY}`
+    ).then((res) => res.data);
+    return data;
+  });
+
+  let temp = meteoData ? kelvinToDegre(meteoData.main.temp) : "inconnu";
+  let weather = meteoData?.weather[0].main;
+
+  //Mettre à jour la ville sélectionnée
+  const handleCountrySelection = (e) => {
+    setSelectedCity(e.target.value);
+  };
+
+  //Recharger l'api à chaque fois qu'on change de ville
+  useEffect(() => {
+    refetchLatLon();
+    refetchMeteo();
+  }, [selectedCity]);
+
   return (
-    <div className="page">Page1</div>
-    // Contient un formulaire (utiliser des librairies vus sur YT) avec des infos sur la personnes qui seront
-    //récupérer et mis à jour sur les différents écrans
-
-    //On pourra utiliser un custom Hook darkmode pour s'entrainer aussi
-    //On pourra aussi utiliser react query pour récupérer des données d'une api gratuite qu'on affichera sur l'une des pages
-  )
-}
-
-export default Page1
+    <div className="page">
+      <select onChange={(e) => handleCountrySelection(e)}>
+        {cities?.map((city) => {
+          return (
+            <option value={city.name} key={city.code}>
+              {city.name}
+            </option>
+          );
+        })}
+      </select>
+      {!isLatLonLoading && !isMeteoLoading ? (
+        <>
+          <p>
+            lat : {lat.toFixed(2)} / lng : {lon.toFixed(2)}
+          </p>
+          <p>Température : {temp}</p>
+          <p>Temps : {weather}</p>
+        </>
+      ) : (
+        <p>loading...</p>
+      )}
+    </div>
+  );
+};
+export default Page1;
