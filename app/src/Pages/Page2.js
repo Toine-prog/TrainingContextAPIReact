ef1d8cfe839904580489fb279f7aa33982d4e1c8
Antoine Ruchot <a.ruchot@outlook.com>
2022-10-20 22:48:54 -0400

    enregistrement des données ok

--- app/src/Pages/Page2.js
+++ app/src/Pages/Page2.js
@@ -3,25 +3,27 @@ import Form from "../components/Form";
 import ResponseForm from "../components/ResponseForm";
 
 const Page2 = () => {
-  const { dataForm, setDataForm } = useState({});
+  const [dataForm, setDataForm] = useState({});
 
   const saveData = (data) => {
-    console.log("data", data);
     setDataForm(data);
   };
   //TODO: Réceptionner les éléments du submit du form pour les transmettre à "ResponseForm"
   return (
     <div className="page">
       <h2>Remplisser le formulaire</h2>
-      <Form saveData={() => saveData()} />
+      <Form saveData={saveData} />
       <div>
         <h2>Vos réponses</h2>
-        <ResponseForm
-          name={dataForm.name}
-          email={dataForm.email}
-          age={dataForm.age}
-          mdp={dataForm.mdp}
-        />
+        {dataForm && (
+          <ResponseForm
+            name={dataForm.name}
+            email={dataForm.email}
+            age={dataForm.age}
+            mdp={dataForm.mdp}
+          />
+        )}{" "}
+        : {}
       </div>
     </div>
   );
